43d0fd1cb603be8c2c453aefdfb948af27a95977
RobinLuoji <330493138@qq.com>
2021-05-25 15:22:05 +0800

    first app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Avatar 
+      url="https://avatars.githubusercontent.com/u/499550?s=88&u=de41ec9325e8a92e281b96a1514a0fd1cd81ad4a&v=4" 
+      :size="50" 
+    />
+    <Avatar 
+      url="https://avatars.githubusercontent.com/u/664177?s=88&u=b213da9f4df34b2fc2063a2038dfd476e54ce4a5&v=4" 
+      :size="100" 
+    />
+    
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Avatar  from "./components/Avatar";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Avatar },
 }
 </script>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,29 @@
+<template>
+  <img 
+    class="avatar-img"
+    :src="url" 
+    :style="{
+      width: size + 'px',
+      height: size + 'px'
+  }">
+</template>
+
+<script>
+export default {
+    props:{
+        url: {
+            type: String,
+            required: true
+        },
+        size:Number,
+    }
+}
+</script>
+
+<style scoped>
+.avatar-img{
+    border-radius: 50%;
+    object-fit: cover; /*避免图片拉伸 */
+    display: block;
+}
+</style>
